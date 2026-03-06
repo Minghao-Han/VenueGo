0817ead0350b31a30bd4bec85d65ff31b85b32bc
Minghao-Han <3508627758@qq.com>
2026-03-05 23:21:31 -0500

    init: ticketing service

---
+++ TicketingService/src/test/java/com/happy/TicketingService/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.TicketingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
